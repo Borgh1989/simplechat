476362d60173b5e2032483e4fadd6dc02311443e
Andrew Yurkevich <borgh1989@mail.ru>
2018-01-02 21:54:26 +0300

    initial commit

---
+++ src/main/java/com/ayurkevich/simplechat/SimplechatApplication.java
@@ -0,0 +1,12 @@
+package com.ayurkevich.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplechatApplication.class, args);
+	}
+}
