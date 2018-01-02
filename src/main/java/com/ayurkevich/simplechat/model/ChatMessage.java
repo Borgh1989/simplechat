0d947678f70d0a4ef9dfccd0e6d1a55ac1bfa4c3
Andrew Yurkevich <borgh1989@mail.ru>
2018-01-02 21:56:07 +0300

    create model

---
+++ src/main/java/com/ayurkevich/simplechat/model/ChatMessage.java
@@ -0,0 +1,36 @@
+package com.ayurkevich.simplechat.model;
+
+
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+}
